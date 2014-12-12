c655fce72655715da6f60824fa8946c6fd30525a
unknown <Aleksei_Ivshin@EPRUIZHW0199.moscow.epam.com>
2014-12-12 16:39:08 +0400

    Replace consumer

--- src/main/java/ru/udm/jms/queue/HelloMessageListner.java
+++ src/main/java/ru/udm/jms/queue/HelloMessageListner.java
@@ -1,21 +1,24 @@
-package ru.udm.consumer;
+package ru.udm.jms.queue;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HelloMessageListner implements MessageListener {
+	private static Logger LOGGER = LoggerFactory
+			.getLogger(HelloMessageListner.class);
 
 	@Override
 	public void onMessage(Message message) {
 		if (message instanceof TextMessage) {
 			try {
-				System.out.println(((TextMessage) message).getText());
+				LOGGER.debug(((TextMessage) message).getText());
 			} catch (JMSException e) {
 				e.printStackTrace();
 			}
